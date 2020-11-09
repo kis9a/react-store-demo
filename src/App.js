f6c7fe9ce69eb09738757ac20a48912b44f88f05
kis9a <kis9ax@gmail.com>
2020-11-09 13:46:28 +0900

    feat: add router

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Provider } from "react-redux";
 import store from "./redux/createStore";
 import InputSection from "./components/InputSection";
 import TodosSection from "./components/TodosSection";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-function App() {
+const ReduxTodos = () => {
   return (
     <Provider store={store}>
       <div className="container">
@@ -12,6 +13,34 @@ function App() {
         <TodosSection />
       </div>
     </Provider>
+  );
+};
+
+const Top = () => {
+  return (
+    <div>
+      <ReduxTodos />
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <React.StrictMode>
+        <div className="header">
+          <Link className="pl" to={"/"}>
+            Home
+          </Link>
+        </div>
+        <div className="app-container">
+          <Switch>
+            <Route path="/" component={Top} exact />
+          </Switch>
+        </div>
+      </React.StrictMode>
+    </Router>
   );
 }
 
